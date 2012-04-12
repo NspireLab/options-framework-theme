922722d3d0fc9e0a707ee70b81419f1666015445
Dallas Bass <hello@nspirelab.com>
2012-04-12 10:36:34 -0500

    test commit

--- functions.php
+++ functions.php
@@ -11,3 +11,19 @@ if ( !function_exists( 'optionsframework_init' ) ) {
 	define( 'OPTIONS_FRAMEWORK_DIRECTORY', get_bloginfo('template_directory') . '/admin/' );
 	require_once dirname( __FILE__ ) . '/admin/options-framework.php';
 }
+
+// Add language supports. Please note that Reverie Framework does not include language files.
+	load_theme_textdomain('reverie', get_template_directory() . '/lang');
+
+// Add post thumbnail supports. http://codex.wordpress.org/Post_Thumbnails
+	add_theme_support('post-thumbnails');
+
+// Add post formarts supports. http://codex.wordpress.org/Post_Formats
+	add_theme_support('post-formats', array('aside', 'gallery', 'link', 'image', 'quote', 'status', 'video', 'audio', 'chat'));
+
+// Add menu supports. http://codex.wordpress.org/Function_Reference/register_nav_menus
+	add_theme_support('menus');
+	register_nav_menus(array(
+	'main_navigation' => __('Main Navigation', 'main_nav'),
+	'second_navigation' => __('Second Navigation', 'second_nav')
+));	
